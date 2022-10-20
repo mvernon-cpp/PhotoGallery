fa23c9d831f6a02721015be050cd77fb4c6ac193
Moina Vernon <mvernon@cpp.edu>
2022-10-20 15:25:20 -0700

    Listing 28.1 - 28.7

--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.photogallery
 
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -52,8 +53,18 @@ class PollWorker(val context: Context, workerParams: WorkerParameters)
                 .build()
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0, notification)
+
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
+
         }
 
         return Result.success()
     }
+    companion object {
+        const val ACTION_SHOW_NOTIFICATION =
+            "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
+        const val PERM_PRIVATE = "com.bignerdranch.android.photogallery.PRIVATE"
+
+    }
+
 }
